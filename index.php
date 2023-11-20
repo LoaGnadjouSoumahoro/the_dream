952bd774662afb587b7063a8b42ce3c5f0d6f655
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2023-11-20 05:41:55 +0100

    affichage des valeurs des champs select

--- index.php
+++ index.php
@@ -2,104 +2,66 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Currency Converter</title>
-    
-
+    <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
 
 <body>
-
     <div class="wrapper">
         <header> Currency Converter</header>
-        <form method="GET" action="./conversion.php" id="conversionForm">
+        <form method="GET" action="" id="conversionForm">
             <div class="amount">
                 <p> Please enter the amount:</p>
-                <input type="text" name="amount" value="0">
+                <input type="text" name="amount" value="<?php echo isset($_GET['amount']) ? htmlspecialchars($_GET['amount']) : ''; ?>">
             </div>
             <div class="drop-list">
                 <div class="from">
-                    <p>From</p>
+                    <p>From:</p>
                     <div class="select-box">
                         <select name="firstcurrency" id="firstcurrency">
-                            <option value="Select">Select</option>
-                            <option value="EUR">EUR</option>
-                            <option value="KWD">KWD</option>
-                            <option value="USD">USD</option>
-                            <option value="CAD">CAD</option>
-                            <option value="ARS">ARS</option>
-                            <option value="XAF">XAF</option>
-                            <option value="BRL">BRL</option>
-                            <option value="BRL">BRL</option>
-                            <option value="TWD">TWD</option>
-
+                            <?php
+                            $currencies = ['Select', 'KWD', 'USD', 'CAD', 'ARS', 'XAF', 'BRL', 'TWD', 'EUR'];
+                            foreach ($currencies as $currency) {
+                                $selected = (isset($_GET['firstcurrency']) && $_GET['firstcurrency'] == $currency) ? 'selected' : '';
+                                echo "<option value=\"$currency\" $selected>$currency</option>";
+                            }
+                            ?>
                         </select>
                     </div>
                 </div>
                 <input type="hidden" name="invert" value="1">
-                <div class="icon"><i class="fa-solid fa-arrow-right-arrow-left"></i></div>
+                <div class="icon">
+                <i class="fa-solid fa-arrow-right"></i>
+                </div>
                 <div class="to">
                     <p> To:</p>
                     <div class="select-box">
                         <select name="secondcurrency" id="secondcurrency">
-                            <option value="Select">Select</option>
-                            <option value="EUR">EUR</option>
-                            <option value="KWD">KWD</option>
-                            <option value="USD">USD</option>
-                            <option value="CAD">CAD</option>
-                            <option value="ARS">ARS</option>
-                            <option value="XAF">XAF</option>
-                            <option value="BRL">BRL</option>
-                            <option value="BRL">BRL</option>
-                            <option value="TWD">TWD</option>
+                            <?php
+                            foreach ($currencies as $currency) {
+                                $selected = (isset($_GET['secondcurrency']) && $_GET['secondcurrency'] == $currency) ? 'selected' : '';
+                                echo "<option value=\"$currency\" $selected>$currency</option>";
+                            }
+                            ?>
                         </select>
                     </div>
                 </div>
             </div>
             <div class="exchange-rate"> 
                 <?php
-
-                    if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET['amount']) && isset($_GET['firstcurrency']) && isset($_GET['secondcurrency'])) {
-                        include('./conversion.php');
-                    } else {
-                        echo 'Getting exchange rate...';
-                    }
+                if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET['amount']) && isset($_GET['firstcurrency']) && isset($_GET['secondcurrency'])) {
+                    include('./conversion.php');
+                } else {
+                    echo 'Getting exchange rate...';
+                }
                 ?> 
             </div>
-            <button type="button" onclick="getExchangeRate()"> Get Exchange Rate</button>
-                
-            </div>
+            <button type="submit"> Get Exchange Rate</button>
         </form>
     </div>
-
-    <!-- <?php
-
-include('./conversion.php');
-    ?> -->
-
-     <script>
-        // Fonction pour effectuer la requête AJAX
-        function getExchangeRate() {
-            var form = document.getElementById('conversionForm');
-            var formData = new FormData(form);
-
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', './conversion.php?' + new URLSearchParams(formData).toString(), true);
-
-            xhr.onload = function () {
-                if (xhr.status == 200) {
-                    // Mettez à jour la div avec le résultat de la conversion
-                    document.querySelector('.exchange-rate').innerHTML = xhr.responseText;
-                }
-            };
-
-            xhr.send();
-        }
-    </script> 
-
 </body>
 
 </html>
